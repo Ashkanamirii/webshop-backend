2e1731277bc828f6bdd150173562f23aa70d2e35
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-06 15:11:29 +0200

    add Payment client

---
+++ src/main/java/com/nackademin/webshopbackend/client/payment/PaymentClient.java
@@ -0,0 +1,11 @@
+package com.nackademin.webshopbackend.client.payment;
+
+/**
+ * Created by Ashkan Amiri
+ * Date:  2021-09-06
+ * Time:  15:11
+ * Project: webshop-backend
+ * Copyright: MIT
+ */
+public class PaymentClient {
+}
