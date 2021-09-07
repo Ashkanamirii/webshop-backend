7e5394a003b7666fb29d7401bdc329e5c79ed9b8
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-08 00:22:33 +0200

    Add cross origin

--- src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
+++ src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
@@ -13,41 +13,42 @@ import javax.servlet.http.HttpServletResponse;
 public class CORSFilter implements Filter {
 
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
 
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
-                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-requested-with, x-auth-token,Jwt-Token");
+		response.setHeader("Access-control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+				"Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-requested-with, x-auth-token,Jwt-Token");
+		response.setHeader("Access-Control-Expose-Headers", "Jwt-Token,access-control-request-headers, access-control-request-method, accept, origin, Authorization, x-requested-with");
 
-        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
-            try {
-                chain.doFilter(req, res);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } else {
-            System.out.println("Pre-flight");
+		if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+			try {
+				chain.doFilter(req, res);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		} else {
+			System.out.println("Pre-flight");
 
-            response.setHeader("Access-Control-Allowed-Methods", "POST, GET, DELETE");
-            response.setHeader("Access-Control-Max-Age", "3600");
-            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type,x-auth-token,Jwt-Token," +
-                    "access-control-request-headers, access-control-request-method, accept, origin, Authorization, x-requested-with");
+			response.setHeader("Access-Control-Allowed-Methods", "POST, GET, DELETE");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "content-type,x-auth-token,Jwt-Token," +
+					"access-control-request-headers, access-control-request-method, accept, origin, Authorization, x-requested-with");
+			response.setHeader("Access-Control-Expose-Headers", "Jwt-Token,access-control-request-headers, access-control-request-method, accept, origin, Authorization, x-requested-with");
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
 
-            response.setStatus(HttpServletResponse.SC_OK);
-        }
+	}
 
-    }
+	public void init(FilterConfig filterConfig) {
+	}
 
-    public void init(FilterConfig filterConfig) {
-    }
-
-    public void destroy() {
-    }
+	public void destroy() {
+	}
 
 }
