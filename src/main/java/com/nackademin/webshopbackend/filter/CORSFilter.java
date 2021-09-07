2e6af6b7f97b04efaa316f20a2e1fe287f37e65a
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-07 14:07:16 +0200

    add token to login api

--- src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
+++ src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
@@ -20,7 +20,7 @@ public class CORSFilter implements Filter {
 
         response.setHeader("Access-control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token,Jwt-Token");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
