30359bfdb479936c859f19d27ba77a8a61d0221f
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-07 22:31:21 +0200

    add consumer for rabbitMQ

---
+++ src/main/java/com/nackademin/webshopbackend/listener/PaymentDTO.java
@@ -0,0 +1,9 @@
+package com.nackademin.webshopbackend.listener;/**
+ * Created by Ashkan Amiri
+ * Date:  2021-09-07
+ * Time:  21:58
+ * Project: webshop-backend
+ * Copyright: MIT
+    
+*/public class PaymentDTO {
+}
