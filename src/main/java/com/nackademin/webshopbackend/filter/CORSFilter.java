f397d3a881c554e84721fe42def0a1948b7250d4
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-07 23:38:02 +0200

    Add cross origin

--- src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
+++ src/main/java/com/nackademin/webshopbackend/filter/CORSFilter.java
@@ -20,9 +20,10 @@ public class CORSFilter implements Filter {
 
         response.setHeader("Access-control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, x-auth-token,Jwt-Token,Authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-requested-with, x-auth-token,Jwt-Token");
 
         if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
             try {
