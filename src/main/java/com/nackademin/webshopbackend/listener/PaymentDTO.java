09acce25744ce4c1ec6b959ce450f49f9f066474
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-07 22:32:01 +0200

    add consumer for rabbitMQ

--- src/main/java/com/nackademin/webshopbackend/listener/PaymentDTO.java
+++ src/main/java/com/nackademin/webshopbackend/listener/PaymentDTO.java
@@ -1,9 +1,40 @@
-package com.nackademin.webshopbackend.listener;/**
+package com.nackademin.webshopbackend.listener;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
  * Created by Ashkan Amiri
  * Date:  2021-09-07
  * Time:  21:58
  * Project: webshop-backend
  * Copyright: MIT
-    
-*/public class PaymentDTO {
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentDTO {
+	@JsonProperty("reference")
+	private String reference;
+	@JsonProperty("paymentId")
+	private String paymentId;
+	@JsonProperty("status")
+	private String status;
+	@JsonCreator
+	public PaymentDTO(@JsonProperty("reference") String reference, @JsonProperty("paymentId") String paymentId,
+	               @JsonProperty("status") String status) {
+		this.reference = reference;
+		this.paymentId = paymentId;
+		this.status = status;
+	}
+	public String getReference() {
+		return reference;
+	}
+
+	public String getPaymentId() {
+		return paymentId;
+	}
+
+	public String getStatus() {
+		return status;
+	}
 }
--- src/main/java/com/nackademin/webshopbackend/listener/RabbitConsumer.java
+++ src/main/java/com/nackademin/webshopbackend/listener/RabbitConsumer.java
@@ -1,5 +1,11 @@
 package com.nackademin.webshopbackend.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
 /**
  * Created by Ashkan Amiri
  * Date:  2021-09-07
@@ -8,12 +14,19 @@ package com.nackademin.webshopbackend.listener;
  * Copyright: MIT
  */
 
+@Component
 public class RabbitConsumer {
 
 
-//	@RabbitListener(queues = "payments")
-//	public void receiveMessage(String message) {
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		Payment payment = objectMapper.readValue(message, Payment.class);
-//	}
+	@RabbitListener(queues = "payments")
+	public void receiveMessage(String message) throws IOException {
+		ObjectMapper o = new ObjectMapper();
+		PaymentDTO payment = o.readValue(message, PaymentDTO.class);
+		System.out.println("Rabbit answer with status-> " + payment.getStatus() + " and ref-> " + payment.getReference());
+		if (payment.getStatus().equals("PAID")) {
+			//TODO: send mail or change status order
+		} else
+			System.out.println("sending to server payments created");
+
+	}
 }
