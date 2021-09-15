6c7deb9453ed94c41efff20e6d19c13076d3f7d7
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-15 13:58:35 +0200

    Add payment client test

--- src/test/java/com/nackademin/webshopbackend/client/payment/PaymentClientTest.java
+++ src/test/java/com/nackademin/webshopbackend/client/payment/PaymentClientTest.java
@@ -1,10 +1,15 @@
 package com.nackademin.webshopbackend.client.payment;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Created by Karl Danielsson - JAVA 20B
@@ -14,12 +19,54 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class PaymentClientTest {
 
-//    @Test
-//    void sendPayment() {
-//        PaymentClient pc = new PaymentClient(new RestTemplate());
-//        String response = pc.sendPayment(new PaymentDto("hej", 20L));
-//        System.out.println(response);
-//
-//        assertEquals("204 NO_CONTENT", response );
-//    }
+	private static final int PORT = 9090;
+	private WireMockServer mockServer;
+	private PaymentDto paymentDto;
+	private PaymentClient paymentClient;
+
+	@BeforeEach
+	public void before() {
+		mockServer = new WireMockServer(PORT);
+		mockServer.start();
+	}
+
+	@AfterEach
+	public void after() {
+		mockServer.stop();
+	}
+
+
+	@Test
+	void sendPaymentSuccessful() {
+		mockServer.stubFor(post(urlEqualTo("/payment")).willReturn(aResponse()
+				.withStatus(HttpStatus.NO_CONTENT.value())));
+
+
+		String paymentServiceUrl = mockServer.baseUrl() + "/payment";
+		RestTemplate restTemplate = new RestTemplate();
+		paymentDto = new PaymentDto("test", 1500);
+		paymentClient = new PaymentClient(restTemplate);
+		paymentClient.setUrl(paymentServiceUrl);
+		String response = paymentClient.sendPayment(paymentDto);
+
+		assertEquals("204 NO_CONTENT", response);
+
+	}
+
+	@Test
+	void sendPaymentUnsuccessfully() {
+		mockServer.stubFor(post(urlEqualTo("/payment")).willReturn(aResponse()
+				.withStatus(HttpStatus.FORBIDDEN.value())));
+
+		String paymentServiceUrl = mockServer.baseUrl() + "/payment";
+		RestTemplate restTemplate = new RestTemplate();
+		paymentDto = new PaymentDto("test", 1500);
+		paymentClient = new PaymentClient(restTemplate);
+		paymentClient.setUrl(paymentServiceUrl);
+
+
+		assertThrows(Exception.class, () -> {
+			paymentClient.sendPayment(paymentDto);
+		});
+	}
 }
